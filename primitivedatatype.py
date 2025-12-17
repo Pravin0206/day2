dff18886893d6cd17e28c8edf6bd839b72d88d1b
Praveen Tangade <pravintangade122@gmail.com>
2025-12-17 11:37:16 +0530

    first commit

---
+++ primitivedatatype.py
@@ -0,0 +1,20 @@
+# Subscripting a String
+
+name = "Rutuja"
+print(name[-1])
+
+#Integers
+
+print(1234)
+
+#Large integers
+
+print(123_456_789)
+
+#Floating Point Numbers
+print(3.14159)
+
+#Booleans
+is_active = True
+is_closed = False
+print(is_active)
