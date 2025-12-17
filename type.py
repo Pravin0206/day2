29361900d0b0c9006340180376107a652c1119bb
Praveen Tangade <pravintangade122@gmail.com>
2025-12-17 11:50:52 +0530

    Add type function examples and type conversion demonstration

---
+++ type.py
@@ -0,0 +1,13 @@
+# Type() function
+
+print(type(42))          # <class 'int'>
+print(type(3.14))       # <class 'float'>
+print(type("Hello"))    # <class 'str'>
+print(type(True))       # <class 'bool'>
+
+
+#Type Conversion
+
+print(int(3.99))        # 3
+
+print("Hello your name is of " + str(len(input("Enter your name: "))) + " characters")  # Converts length to string for concatenation
